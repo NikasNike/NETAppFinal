0a5e28f55e0e15358a078b01d7bab3ce26bc4b4e
NikasNike <92685343+NikasNike@users.noreply.github.com>
2024-06-03 19:52:30 +0300

    Delete ConsoleApp1 directory

--- ConsoleApp1/Inter.cs
+++
@@ -1,7 +0,0 @@
-﻿public interface IMessageSourceClient
-{
-    void Connect(string address);
-    void Disconnect();
-    void SendMessage(string message);
-    event EventHandler<string> MessageReceived;
-}
